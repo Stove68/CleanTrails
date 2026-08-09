0dc15a9551bfdd9ee8ce0189d0b09dd57c1d89d3
Stove68 <joerg-stoverock@idexx.com>
2026-08-09 13:49:04 +0200

    Initialize map with OpenStreetMap tiles and marker

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const map = L.map("map").setView([51.1657, 10.4515], 6);
 
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors"
+}).addTo(map);
+
+L.marker([51.1657, 10.4515])
+    .addTo(map)
+    .bindPopup("CleanTrails")
+    .openPopup();
