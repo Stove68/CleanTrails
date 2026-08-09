09ab8d894cfc8eb341a36f0dffaa5b88d4a11cde
Stove68 <joerg-stoverock@idexx.com>
2026-08-09 13:55:57 +0200

    Add geolocation feature to display current location

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
 
-L.marker([51.1657, 10.4515])
-    .addTo(map)
-    .bindPopup("CleanTrails")
-    .openPopup();
+let currentMarker = null;
+
+navigator.geolocation.getCurrentPosition(
+    function (position) {
+
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        map.setView([lat, lng], 16);
+
+        currentMarker = L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup("Mein Standort")
+            .openPopup();
+    },
+
+    function () {
+        alert("Standortfreigabe wurde nicht erteilt.");
+    }
+);
